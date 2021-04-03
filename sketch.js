408b0fe53448398be4cecfe0864ade108f0eedfa
BananaApple-Coder <81397675+BananaApple-Coder@users.noreply.github.com>
2021-04-02 20:54:52 -0500

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,21 +2,19 @@ var sr, mr
 var a, b
 
 function setup() {
-  createCanvas(1200,800);
-  sr = createSprite(200, 200, 50, 80);
-  mr = createSprite(400, 200, 80, 30);
+  createCanvas(windowWidth,windowHeight);
+  sr = createSprite(width/2, height/2, width/24, height/10);
+  mr = createSprite(width, height, width/15, height/27);
   sr.shapeColor = "red"
   mr.shapeColor = "red"
-  sr.debug = true
-  mr.debug = true
 }
 
 function draw() {
   background(0,0,0); 
   mr.x = World.mouseX
   mr.y = World.mouseY 
-  sr.width = sin(frameCount) * 50 + 60
-  sr.height = cos(frameCount) * 80 + 90
+  sr.width = sin(frameCount) * width/24 + width/24 + width/120
+  sr.height = cos(frameCount) * height/10 + height/10 + height/80
   a = mr.width/2 + sr.width/2
   b = mr.height/2 + sr.height/2
   if (abs(mr.x - sr.x) < a && abs(mr.y - sr.y) < b) {
